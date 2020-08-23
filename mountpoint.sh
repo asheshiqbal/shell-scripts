ef5b2280a60db4b47a08ab3ea656a8830e5d67f8
Ashesh Iqbal <33754010+asheshiqbal@users.noreply.github.com>
2020-08-23 23:33:13 +0600

    Update mountpoint.sh

--- mountpoint.sh
+++ mountpoint.sh
@@ -1,38 +1,39 @@
-# Mount - attachment of a filesystem to the directory structure
-# Mount Point - used by UNIX-based OS in place of a drive-letter, can be either local or network share (conventionally /mnt or /media)
+## Mount - attachment of a filesystem to the directory structure
+## Mount Point - used by UNIX-based OS in place of a drive-letter, can be either local or network share (conventionally /mnt or /media)
 
 sudo su -
 
+useradd ashesh
+passwd ashesh
 useradd oracle
 passwd oracle
 
 mkdir /oracle 
 mkdir /backup
 
-mount /dev/partition-name /oracle/
-mount /dev/partition-name /backup/
-df -hT
-
-### umount /dev/partition-name or //mount-point/
-
-### Persistent Mounting:
-cat /etc/fstab
-blkid /dev/oracle
-blkid /dev/backup
-vim /etc/fstab
-### append:
-UUID=XXXX /oracle
-UUID=XXXX /backup
-
 fdisk -l
 
-cat /proc/partitions
+fdisk /dev/sdb
+partprobe
+mke2fs -j /dev/sdb
 
-cat /etc/mtab
+fdisk /dev/sdc
+partprobe
+mke2fs -j /dev/sdc
 
+mount /dev/sdb /oracle
+mount /dev/sdc /backup
 
-chmod 700 -R /oracle
-chmod 700 -R /backup
+vim /etc/fstab
+	/dev/sdb /oracle ext3 defaults 1 2
+	/dev/sdc /oracle ext3 defaults 1 2
+
+cat /etc/fstab
+
+df -hT
 
 chown -R oracle /oracle
 chown -R ashesh /backup
+
+chmod -R 700 /oracle
+chmod -R 700 /backup
